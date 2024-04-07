de41a689bb7a2735b3eb6f6daff77f00c2136927
Asimsaeed111 <161206949+Asimsaeed111@users.noreply.github.com>
2024-04-07 02:36:58 -0700

    this my currency convertor application

---
+++ main.js
@@ -0,0 +1,29 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+let currency = {
+    USD: 1,
+    GPB: 0.76,
+    EUR: 0.91,
+    INR: 74.57,
+    PKR: 280
+};
+let answer = await inquirer.prompt([
+    {
+        name: "from", message: "enter your currency from", type: "list", choices: ["USD", "GPB", "EUR", "INR", "PKR"]
+    },
+    {
+        name: "to", message: "enter your currency to", type: "list", choices: ["USD", "GPB", "EUR", "INR", "PKR"]
+    },
+    {
+        name: "amount", message: "enter your amount", type: "number"
+    }
+]);
+// let fromcurrency = answer.from
+// let tocurrency = answer.to
+let fromamount = currency[answer.from];
+let toamount = currency[answer.to];
+let amount = answer.amount;
+let baseamount = amount / fromamount;
+let convertedamount = Math.round(baseamount * toamount);
+console.log(convertedamount);
+// Here usd is our base currency. 
